dac7f0c06f3913b767f24fe3a7f9c5423e72ce91
ShoKubota <sh2o.1114@mineo.jp>
2022-01-15 16:35:06 +0900

    add: enumを導入 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 225 }
   validates :email, presence: true, uniquness: true
+
+  enum role: {
+    general: 0,
+    admin: 1
+  }
 end
