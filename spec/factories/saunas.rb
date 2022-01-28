f2e7037669698ca89585440a95aa08473a376438
ShoKubota <sh2o.1114@mineo.jp>
2022-01-28 17:55:07 +0900

    add: factorybotでテストデータを作成 #4

---
+++ spec/factories/saunas.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :sauna do
+    type { "Sauna" }
+    sequence(:name) { |n| "スーパー銭湯#{n}店" }
+    rating { 3.6 }
+    opening_hours { "[\"月曜日: 10時00分～22時00分\", \"火曜日: 10時00分～22時00分\", \"水曜日: 10時00分～22時00分\", \"木曜日: 10時00分～22時00分\", \"金曜日: 10時00分～22時00分\", \"土曜日: 10時00分～22時00分\", \"日曜日: 10時00分～22時00分\"]" }
+    tel_number { "03-9876-5432" }
+    place_id { "sauna_place_id" }
+  end
+end
