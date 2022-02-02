d5556f1690e1c7a873a6b2ed5a4da4380315706d
ShoKubota <sh2o.1114@mineo.jp>
2022-02-02 14:33:23 +0900

    add: index#showを作成 #9

--- app/controllers/jiros_controller.rb
+++ app/controllers/jiros_controller.rb
@@ -5,5 +5,7 @@ class JirosController < ApplicationController
   end
 
   def show
+    @jiro = Jiro.find_by(id: params[:id])
+    @saunas = Sauna.all.within(1.5, origin: [@jiro.latitude, @jiro.longitude]).by_distance(origin: [@jiro.latitude, @jiro.longitude])
   end
 end
