71f2ffc5560f8b8f36e91cdbc973519848f90e6f
ShoKubota <sh2o.1114@mineo.jp>
2022-02-05 19:12:22 +0900

    add: rails-adminを使用して管理画面の作成 #8

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,6 @@
+const webpack = require('webpack');
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+environment.plugins.append('ProvidePlugin-jQuery', new webpack.ProvidePlugin({jQuery: 'jquery'}));
 
 module.exports = environment
