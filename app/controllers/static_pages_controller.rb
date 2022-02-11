2646daf43cf4908bbcd5cb6bb26bacdc242aad37
ShoKubota <sh2o.1114@mineo.jp>
2022-02-11 17:19:47 +0900

    add: プライバシーポリシーと利用規約を追加 #15

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,9 @@ class StaticPagesController < ApplicationController
     @jiros = Jiro.all
     @saunas = Sauna.all
   end
+
+  def terms; end
+
+  def privacy_policy; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#top'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
   resources :jiros, only: %i[index show]
   get 'maps', to: 'maps#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
