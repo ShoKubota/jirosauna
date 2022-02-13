77fe32965c58c5cf834e468232e027244baec164
ShoKubota <sh2o.1114@mineo.jp>
2022-02-13 19:45:09 +0900

    fix: postcss-loader 'config' validation error

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+function hotfixPostcssLoaderConfig (subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+}
+
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
 
 module.exports = environment
