b895e5354f0e134920d4d64d7a4b1f6e7f5cd216
ShoKubota <sh2o.1114@mineo.jp>
2022-02-14 17:10:28 +0900

    add: metaタグ設定 #64

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,33 @@ module ApplicationHelper
       'shadow-lg bg-opacity-90'
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'ジロサウナ〜',
+      title: '二郎とサウナを同時に探せるサイト',
+      reverse: true,
+      separator: '|',
+      description: '「ジロサウナ〜」は、首都圏400件以上の二郎系ラーメンとサウナを網羅した二郎とサウナを同時に探せる検索サイト。マップや店舗から最高の組み合わせを探すことができます。',
+      keywords: 'ラーメン, 二郎, 二郎系, サウナ, 銭湯, 整う',
+      canonical: request.original_url,
+      icon: [
+        { href: image_url('logo_main.png'), sizes: '32x32' },
+        { href: image_url('logo_main.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: 'ジロサウナ〜',
+        title: '二郎とサウナを同時に探せるサイト',
+        description: '「ジロサウナ〜」は、首都圏400件以上の二郎系ラーメンとサウナを網羅した二郎とサウナを同時に探せる検索サイト。マップや店舗から最高の組み合わせを探すことができます。', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@botcher_matsu',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
