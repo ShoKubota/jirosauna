ed80d75f257b77b0f28273c40a09b83f9ad19f8b
ShoKubota <sh2o.1114@mineo.jp>
2022-03-23 22:27:55 +0900

    add: ログインに関するテストを追加 #5

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "銭湯二郎#{n}号" }
-    sequence(:email) { |n| "jiro{n}@example.com"}
+    sequence(:email) { |n| "jiro#{n}@example.com"}
     password { 'password' }
     password_confirmation { 'password' }
     role { 0 }
